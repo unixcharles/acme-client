004e19cbf5cb1979de752074a9fd2d970d295938
Jonne Haß <me@jhass.eu>
2015-12-07 03:33:52 +0100

    Fix fullchain certificate order
    fixes #28

--- lib/acme/client.rb
+++ lib/acme/client.rb
@@ -44,7 +44,7 @@ class Acme::Client
     }
 
     response = connection.post(@operation_endpoints.fetch('new-cert'), payload)
-    ::Acme::Certificate.new(OpenSSL::X509::Certificate.new(response.body), fetch_chain(response).reverse)
+    ::Acme::Certificate.new(OpenSSL::X509::Certificate.new(response.body), fetch_chain(response))
   end
 
   def fetch_chain(response, limit=10)
