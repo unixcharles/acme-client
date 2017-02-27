7cad64b2a2e4977346c0f2dcc9cc8909304b6529
Bart de Water <bart.dewater@shopify.com>
2017-02-27 15:54:47 -0500

    Check `OpenSSL::VERSION` instead of `respond_to?`/`is_a?` to find ASN1 data

--- spec/support/asn1_helper.rb
+++ spec/support/asn1_helper.rb
@@ -1,11 +1,9 @@
 module Asn1Helper
-  def asn1_dig(obj)
-    if obj.respond_to?(:first) && obj.first.is_a?(Enumerable)
-      asn1_dig obj.first
-    elsif obj.respond_to?(:value) && obj.value.is_a?(Enumerable)
-      asn1_dig obj.value
+  def asn1_dig(attribute)
+    if OpenSSL::VERSION < '2.0.0'
+      attribute.value.first.first.value
     else
-      obj
+      attribute.value.value.first.value.first.value
     end
   end
 end
