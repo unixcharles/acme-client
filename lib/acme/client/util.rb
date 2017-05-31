96f3a81d3491f1ac60f90e98193c77d07d75ee19
Ben Toews <mastahyeti@gmail.com>
2017-05-31 14:16:12 -0600

    move some test helpers to ssl_helper

--- lib/acme/client/util.rb
+++ lib/acme/client/util.rb
@@ -20,40 +20,5 @@ module Acme::Client::Util
     end
   end
 
-  # Verify a CSR's signature.
-  #
-  # csr  - A OpenSSL::X509::Request instance.
-  # priv - An OpenSSL::PKey::EC or OpenSSL::PKey::RSA instance.
-  #
-  # Returns boolean.
-  def verify_csr(csr, priv)
-    case priv
-    when OpenSSL::PKey::EC
-      csr.verify(priv)
-    when OpenSSL::PKey::RSA
-      csr.verify(priv.public_key)
-    else
-      raise ArgumentError, 'priv must be EC or RSA'
-    end
-  end
-
-  # Export a private key's public key in DER format.
-  #
-  # priv - An OpenSSL::PKey::EC or OpenSSL::PKey::RSA instance.
-  #
-  # Returns a String.
-  def public_key_to_der(priv)
-    case priv
-    when OpenSSL::PKey::EC
-      dup = OpenSSL::PKey::EC.new(priv.to_der)
-      dup.private_key = nil
-      dup.to_der
-    when OpenSSL::PKey::RSA
-      priv.public_key.to_der
-    else
-      raise ArgumentError, 'priv must be EC or RSA'
-    end
-  end
-
   extend self
 end
