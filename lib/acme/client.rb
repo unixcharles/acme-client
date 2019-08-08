7ab821ad214c11eba79d82c43e4e8b3a6399b7a0
Andrei Bocan <zmaxor@gmail.com>
2019-08-07 22:08:28 -0700

    Client: Add bad_nonce_retry as an option

--- lib/acme/client.rb
+++ lib/acme/client.rb
@@ -29,7 +29,7 @@ class Acme::Client
     pem: 'application/pem-certificate-chain'
   }
 
-  def initialize(jwk: nil, kid: nil, private_key: nil, directory: DEFAULT_DIRECTORY, connection_options: {})
+  def initialize(jwk: nil, kid: nil, private_key: nil, directory: DEFAULT_DIRECTORY, connection_options: {}, bad_nonce_retry: 0)
     if jwk.nil? && private_key.nil?
       raise ArgumentError, 'must specify jwk or private_key'
     end
@@ -41,6 +41,7 @@ class Acme::Client
     end
 
     @kid, @connection_options = kid, connection_options
+    @bad_nonce_retry = bad_nonce_retry
     @directory = Acme::Client::Resources::Directory.new(URI(directory), @connection_options)
     @nonces ||= []
   end
@@ -280,12 +281,12 @@ class Acme::Client
 
   def new_connection(endpoint:)
     Faraday.new(endpoint, **@connection_options) do |configuration|
-      configuration.request(:retry, max: 10,
+      configuration.request(:retry, max: @bad_nonce_retry,
                             interval: 0.05,
                             interval_randomness: 0.5,
                             backoff_factor: 2,
                             methods: Faraday::Connection::METHODS,
-                            exceptions: [Acme::Client::Error::BadNonce])
+                            exceptions: [Acme::Client::Error::BadNonce]) if @bad_nonce_retry > 0
       yield(configuration) if block_given?
       configuration.adapter Faraday.default_adapter
     end
